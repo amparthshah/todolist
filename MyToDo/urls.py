b1b56be5bc3855d486b09135b9338f4f6d4295e2
Parth <shahparth050102@gmail.com>
2021-11-28 15:37:34 +0530

    Fianl Commit

--- MyToDo/urls.py
+++ MyToDo/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+
+admin.site.site_header = "ToDoList By Parth Shah"
+admin.site.site_title = "ToDoList"
+admin.site.index_title = "ToDoList"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ToDoApp.urls')),
